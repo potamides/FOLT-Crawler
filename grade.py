adf31f8c357f4d497845d5ab830b00a9df4adbb9
Jonas Belouadi <jonas.belouadi@posteo.net>
2021-12-08 15:48:49 +0100

    Fix erroneous assert statement.

--- grade.py
+++ grade.py
@@ -3,7 +3,7 @@ from sys import argv
 from crawl import read_tweets
 from sklearn.metrics import cohen_kappa_score
 
-def read_submission(filename):
+def read_predictions(filename):
     predictions = list()
     with open(filename, "rb") as f:
         for line in f:
@@ -14,9 +14,10 @@ assert len(argv[1:]) == 3, "Only two arguments allowed!"
 label_file, submission_file, prediction_file = argv[1], argv[2], argv[3]
 
 id2labels = {tweet['id']:int(tweet['label']) for tweet in read_tweets(label_file)}
-id2pred = {tweet['id']:pred for tweet, pred in zip(read_tweets(submission_file), read_submission(prediction_file))}
+submission_content, prediction_content = read_tweets(submission_file), read_predictions(prediction_file)
+id2pred = {tweet['id']:pred for tweet, pred in zip(submission_content, prediction_content)}
 
-assert len(id2labels) == len(id2pred), "Amount of annotations differ!"
+assert len(submission_content) == len(prediction_content), "Wrong amount of annotations!"
 assert 0 <= min(id2pred.values()) <= max(id2pred.values()) <= 2, "Invalid category"
 
 labels, predictions = list(), list()
